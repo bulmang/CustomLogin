533487d28c314b106980507ebb1171b3322d4b15
Bulmang <hmk113737@gmail.com>
2022-10-06 23:41:02 +0900

    Initial Commit

---
+++ CustomLogin/CustomLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomLoginApp.swift
+//  CustomLogin
+//
+//  Created by 하명관 on 2022/10/06.
+//
+
+import SwiftUI
+
+@main
+struct CustomLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
